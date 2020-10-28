0ff9a1cf50debf1b156b9aae45792f0439334603
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-10-28 19:14:31 +0530

    Added random load balancing algo
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- loadBalanceNode.py
+++ loadBalanceNode.py
@@ -1,6 +1,18 @@
 import SimpleHTTPServer
 import SocketServer
 import sys
+import random
+
+def RR(c, arr):
+	if (c % 2 == 0):
+		add = arr[0]
+	else:
+		add = arr[1]
+	return add
+
+def Random(arr):
+	n = random.randint(0,1)
+	return arr[n]
 
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	arr = ["http://10.0.0.5:8080", "http://10.0.0.6:8080"]	
@@ -8,17 +20,14 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		global c
 		self.send_response(301)
-       		if (sys.argv[1] == "bal"):
-			if (c % 2 == 0 ):
-				add = self.arr[0]
-			else:
-				add = self.arr[1]
+		if (sys.argv[1] == "bal"):
+			add = Random(self.arr)
 		else:
 			add = self.arr[0]
 		c = c + 1
 		#print(add)
-        	self.send_header('Location', add)
-        	self.end_headers()
+		self.send_header('Location', add)
+		self.end_headers()
 
 c = 0
 
